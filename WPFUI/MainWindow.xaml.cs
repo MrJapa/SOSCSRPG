717a0dafc9c0209d9535e18ac50f516f8eafbf04
Jacob Japa <jjv@live.dk>
2021-10-18 14:19:04 +0200

    Added Engine with ViewModels and Player models

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace WPFUI
         public MainWindow()
         {
             InitializeComponent();
+            Cursor = new Cursor(".\\Cursor\\katana.cur");
         }
     }
 }
